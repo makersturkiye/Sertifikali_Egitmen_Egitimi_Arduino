42c0546d7f44daddaca43d3e7b19dc59aca2e1c9
Makers Türkiye <43065243+makersturkiye@users.noreply.github.com>
2019-02-16 12:00:19 +0200

    Update rgb_led.ino

--- Arduino IDE/05-RGB-LED-Kontrolu/rgb_led/rgb_led.ino
+++ Arduino IDE/05-RGB-LED-Kontrolu/rgb_led/rgb_led.ino
@@ -1,7 +1,10 @@
 int red   = 9;
 int blue  = 10;
 int green = 11;
+
 int veri = 0;
+
+
 void setup() {
   pinMode(red, OUTPUT);
   pinMode(blue, OUTPUT);
@@ -10,26 +13,23 @@ void setup() {
 
 void loop() {
 
-   veri = random(0, 255); 
-   
+   analogWrite(red, 255);
+   analogWrite(blue, veri);
+   analogWrite(green, 255);
+   delay(5);
+
    analogWrite(red, 0);
    analogWrite(blue, veri);
-   analogWrite(green, 0);
-   delay(1000);
+   analogWrite(green, 255- veri);
+   delay(5);
 
-   veri = random(0, 255); 
-   
-   analogWrite(red, 0);
-   analogWrite(blue, 0);
-   analogWrite(green, veri);
-   delay(1000);
 
-   veri = random(0, 255); 
-   
-   analogWrite(red, veri);
-   analogWrite(blue, 0);
+   analogWrite(red, 255-veri);
+   analogWrite(blue, veri);
    analogWrite(green, 0);
-   delay(1000);
+   delay(5);
+   veri++;
+    
 
 
   
