3fd135a010a4d35af9a2f500e1d400e122f368df
Makers Türkiye <43065243+makersturkiye@users.noreply.github.com>
2019-02-16 13:01:13 +0200

    Update isikDegeri.ino

--- Arduino IDE/04-Isik-Seviyesi-Olcme/isikDegeri/isikDegeri.ino
+++ Arduino IDE/04-Isik-Seviyesi-Olcme/isikDegeri/isikDegeri.ino
@@ -1,6 +1,8 @@
 // Esik degeri belirleme
 int esikDegeri = 100;
 
+int isikDegeri;
+
 // LED'in bağlı olduğu pin numarası
 int led_pin = 5;
 
@@ -12,13 +14,13 @@ void setup() {
 void loop() {
   // isikDegeri adında bir değişken oluşturduk
   // Analog okuma yapıp bu değişkene atadık
-  int isikDegeri = analogRead(A0);
+  isikDegeri = analogRead(A0);
 
   // Ekrana okunan analog degeri yazdırıyoruz
   Serial.println(isikDegeri);
 
   // Eger esik degerinden büyükse led yansın
-  if(deger > esikDegeri){
+  if(isikDegeri > esikDegeri){
      digitalWrite(led_pin, HIGH);     
   }
   // eger esik degerinden küçükse led sönsün.
